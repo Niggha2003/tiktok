642b86842dfbec93613a96f6e22efd34c7735377
Niggha2003 <nguyennghia241003@gmail.com>
2023-06-18 09:42:28 +0700

    Cấu hình layout và router cho dự án

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from './components/Layout';
+
 function App() {
     return (
-        <div className="App" style={{ padding: '30px' }}>
-            Helelllelel
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) {
+                            Layout = route.layout;
+                        }
+
+                        if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
