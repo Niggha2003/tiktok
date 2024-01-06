a175254e1dfb68f07e03bd2ae3d89037d02f8fa2
Niggha2003 <nguyennghia241003@gmail.com>
2024-01-07 00:01:44 +0700

    test conflict

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from '~/routes';
 import { DefaultLayout } from '~/layouts';
-
+console.log(100);
 function App() {
     return (
         <Router>
